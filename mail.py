27961e76690315a4f10cba29e65e5d8a0f131e5b
Sai <sai@localhost.localdomain>
2020-02-20 21:17:40 +0530

    mailaccount created and added

--- mail.py
+++ mail.py
@@ -1,15 +1,15 @@
 import smtplib
 
 def email_send(details,r_name,r_email,organ):
-    fromaddr = "sc.balli22@gmail.com"  
+    fromaddr = "chaindonars20@gmail.com"  
     toaddrs  = r_email 
     SUBJECT="Alert! We have found a donor for "+organ
     TEXT= "Hello "+r_name+",\n"+"Hope you are doing well, we have found a donor for "+organ+"\nand the donors Details are: \n"+"   Name= "+details[1]+"\n   Mobile no= "+details[2]+"\n   Address= "+details[3]+"\n   Age= "+details[6]
     
     msg = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)  
 
-    username = 'sc.balli22'  
-    password = 'Balli009'
+    username = 'chaindonars20'  
+    password = 'chain2020'
     server = smtplib.SMTP('smtp.gmail.com', 587)  
     server.ehlo()
     server.starttls()
