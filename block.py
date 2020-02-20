e2ecdc3ff656a8919fb81ac9623e7dd7a95939d2
Sai <sai@localhost.localdomain>
2020-02-20 09:30:04 +0530

    initial prer

---
+++ block.py
@@ -0,0 +1,35 @@
+def donor_details():
+    pass
+
+def recipient_details():
+    pass
+
+def match_organs():
+    pass
+
+def check_connection():
+    pass
+
+def status():
+    pass
+
+def options():
+    print("1: Enter Donor's Details")
+    print("2: Enter Recipient details")
+    print("3: show matched patients")
+    print("4: check connection and verification")
+    print("5: status")
+    ip=int(input(""))
+    if(ip==1):
+        def donor_details()
+    if(ip==2):
+        def recipient_details()
+    if(ip==3):
+        def match_organs()
+    if(ip==4):
+        def check_connection()
+    if(ip==5):
+        def status()
+    
+
+    
